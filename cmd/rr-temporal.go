84650d873a0f58a44fae77a2f0df67b019a917d3
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-10-16 13:04:58 +0300

    Remove discuss notes

--- cmd/rr-temporal.go
+++ cmd/rr-temporal.go
@@ -9,15 +9,6 @@ import (
 )
 
 func main() {
-	/*
-		1. -c flag - path to the config (default .rr.yaml
-		2. change working dir to the path with .rr.yaml
-		3. init endure here, path should be -c
-		4. command to serve endure container
-		5. plugins to use - temporal, config, factory
-		6. USE COBRA!!!!!11111oneoneone
-	*/
-
 	var err error
 	subcommands.Container, err = endure.NewContainer(endure.DebugLevel, endure.RetryOnFail(false))
 	if err != nil {
