f2c7750701137efee0db32a649e991b4ab745e86
Wolfy-J <wolfy.jd@gmail.com>
2020-12-22 21:32:15 +0300

    - empty workflow test

--- tests/query_test.go
+++ tests/query_test.go
@@ -1,61 +1,53 @@
 package tests
 
-import (
-	"context"
-	"github.com/stretchr/testify/assert"
-	"go.temporal.io/sdk/client"
-	"testing"
-	"time"
-)
-
-func Test_ListQueries(t *testing.T) {
-	s := NewTestServer()
-	defer s.MustClose()
-
-	w, err := s.Client().ExecuteWorkflow(
-		context.Background(),
-		client.StartWorkflowOptions{
-			TaskQueue: "default",
-		},
-		"QueryWorkflow",
-		"Hello World",
-	)
-	assert.NoError(t, err)
-
-	time.Sleep(time.Millisecond * 500)
-
-	v, err := s.Client().QueryWorkflow(context.Background(), w.GetID(), w.GetRunID(), "error", -1)
-	assert.Nil(t, v)
-	assert.Error(t, err)
-
-	assert.Contains(t, err.Error(), "KnownQueryTypes=[get]")
-}
-
-func Test_GetQuery(t *testing.T) {
-	s := NewTestServer()
-	defer s.MustClose()
-
-	w, err := s.Client().ExecuteWorkflow(
-		context.Background(),
-		client.StartWorkflowOptions{
-			TaskQueue: "default",
-		},
-		"QueryWorkflow",
-		"Hello World",
-	)
-	assert.NoError(t, err)
-
-	err = s.Client().SignalWorkflow(context.Background(), w.GetID(), w.GetRunID(), "add", 88)
-	assert.NoError(t, err)
-	time.Sleep(time.Millisecond * 500)
-
-	v, err := s.Client().QueryWorkflow(context.Background(), w.GetID(), w.GetRunID(), "get", nil)
-	assert.NoError(t, err)
-
-	var r int
-	assert.NoError(t, v.Get(&r))
-	assert.Equal(t, 88, r)
-}
+//func Test_ListQueries(t *testing.T) {
+//	s := NewTestServer()
+//	defer s.MustClose()
+//
+//	w, err := s.Client().ExecuteWorkflow(
+//		context.Background(),
+//		client.StartWorkflowOptions{
+//			TaskQueue: "default",
+//		},
+//		"QueryWorkflow",
+//		"Hello World",
+//	)
+//	assert.NoError(t, err)
+//
+//	time.Sleep(time.Millisecond * 500)
+//
+//	v, err := s.Client().QueryWorkflow(context.Background(), w.GetID(), w.GetRunID(), "error", -1)
+//	assert.Nil(t, v)
+//	assert.Error(t, err)
+//
+//	assert.Contains(t, err.Error(), "KnownQueryTypes=[get]")
+//}
+//
+//func Test_GetQuery(t *testing.T) {
+//	s := NewTestServer()
+//	defer s.MustClose()
+//
+//	w, err := s.Client().ExecuteWorkflow(
+//		context.Background(),
+//		client.StartWorkflowOptions{
+//			TaskQueue: "default",
+//		},
+//		"QueryWorkflow",
+//		"Hello World",
+//	)
+//	assert.NoError(t, err)
+//
+//	err = s.Client().SignalWorkflow(context.Background(), w.GetID(), w.GetRunID(), "add", 88)
+//	assert.NoError(t, err)
+//	time.Sleep(time.Millisecond * 500)
+//
+//	v, err := s.Client().QueryWorkflow(context.Background(), w.GetID(), w.GetRunID(), "get", nil)
+//	assert.NoError(t, err)
+//
+//	var r int
+//	assert.NoError(t, v.Get(&r))
+//	assert.Equal(t, 88, r)
+//}
 
 //
 //func Test_SendSignalDuringTimer(t *testing.T) {
