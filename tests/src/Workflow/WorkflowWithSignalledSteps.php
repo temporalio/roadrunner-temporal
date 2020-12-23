85b1589d472e7c2402e063e960908905922a3875
Wolfy-J <wolfy.jd@gmail.com>
2020-12-23 15:00:51 +0300

    - workflow with signals, queries and steps

--- tests/src/Workflow/WorkflowWithSignalledSteps.php
+++ tests/src/Workflow/WorkflowWithSignalledSteps.php
@@ -5,8 +5,10 @@ namespace Temporal\Tests\Workflow;
 
 use React\Promise\Deferred;
 use React\Promise\PromiseInterface;
+use Temporal\Client\Activity\ActivityOptions;
 use Temporal\Client\Workflow;
 use Temporal\Client\Workflow\WorkflowMethod;
+use Temporal\Tests\Activity\SimpleActivity;
 use function Symfony\Component\String\s;
 
 class WorkflowWithSignalledSteps
@@ -14,6 +16,11 @@ class WorkflowWithSignalledSteps
     #[WorkflowMethod(name: 'WorkflowWithSignalledSteps')]
     public function handler()
     {
+        $simple = Workflow::newActivityStub(
+            SimpleActivity::class,
+            ActivityOptions::new()->withStartToCloseTimeout(5)
+        );
+
         $value = 0;
         Workflow::registerQuery('value', function () use (&$value) {
             return $value;
