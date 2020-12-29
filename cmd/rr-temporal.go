3700378f0e3be80416ec63516557235dee73040f
Wolfy-J <wolfy.jd@gmail.com>
2020-12-29 15:35:18 +0300

    - restore CLI based on RR2

--- cmd/rr-temporal.go
+++ cmd/rr-temporal.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"log"
-	"os"
-	"os/signal"
-	"sync"
-	"syscall"
-
 	"github.com/spiral/endure"
-	"github.com/spiral/roadrunner/v2/plugins/config"
 	"github.com/spiral/roadrunner/v2/plugins/informer"
 	"github.com/spiral/roadrunner/v2/plugins/resetter"
+	"log"
+
+	"github.com/spiral/roadrunner/v2/cmd/cli"
 
 	"github.com/spiral/roadrunner/v2/plugins/logger"
 	"github.com/spiral/roadrunner/v2/plugins/rpc"
@@ -21,17 +17,18 @@ import (
 )
 
 func main() {
-	container, err := endure.NewContainer(nil, endure.RetryOnFail(false))
+	var err error
+	cli.Container, err = endure.NewContainer(
+		nil,
+		endure.SetLogLevel(endure.ErrorLevel),
+		endure.RetryOnFail(false),
+	)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	cfg := &config.Viper{}
-	cfg.Path = ".rr.yaml"
-	cfg.Prefix = "rr"
-
-	err = container.RegisterAll(
-		cfg,
+	err = cli.Container.RegisterAll(
 		&logger.ZapLogger{},
 
 		// Helpers
@@ -47,57 +44,9 @@ func main() {
 		&activity.Plugin{},
 		&workflow.Plugin{},
 	)
-
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	err = container.Init()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	ch, err := container.Serve()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-
-	stopCh := make(chan struct{}, 1)
-
-	go func() {
-		defer wg.Done()
-		for {
-			select {
-			case e := <-ch:
-				err = container.Stop()
-				if err != nil {
-					log.Fatal(e.Error.Error() + ":" + err.Error())
-				}
-				log.Fatal(e.Error)
-			case <-sig:
-				err = container.Stop()
-				if err != nil {
-					log.Fatal(err)
-				}
-				return
-			case <-stopCh:
-				// timeout
-				err = container.Stop()
-				if err != nil {
-					log.Fatal(err)
-				}
-				return
-			}
-		}
-	}()
-
-	stopCh <- struct{}{}
-	wg.Wait()
+	cli.Execute()
 }
