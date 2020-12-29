9e338b47d2e4cba67914656bdb311080f1d7c770
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-12-29 13:41:17 +0300

    Remove CLI

--- cmd/rr-temporal.go
+++ cmd/rr-temporal.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"github.com/spiral/endure"
+	"github.com/spiral/roadrunner/v2/plugins/config"
 	"github.com/spiral/roadrunner/v2/plugins/informer"
 	"github.com/spiral/roadrunner/v2/plugins/resetter"
 
@@ -13,19 +18,20 @@ import (
 	"github.com/temporalio/roadrunner-temporal/plugins/activity"
 	"github.com/temporalio/roadrunner-temporal/plugins/temporal"
 	"github.com/temporalio/roadrunner-temporal/plugins/workflow"
-
-	"github.com/temporalio/roadrunner-temporal/cmd/cli"
 )
 
 func main() {
-	var err error
-	cli.Container, err = endure.NewContainer(cli.InitLogger(), endure.RetryOnFail(false))
+	container, err := endure.NewContainer(nil, endure.RetryOnFail(false))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = cli.Container.RegisterAll(
-		// todo: move to root
+	cfg := &config.Viper{}
+	cfg.Path = ".rr.yaml"
+	cfg.Prefix = "rr"
+
+	err = container.RegisterAll(
+		cfg,
 		&logger.ZapLogger{},
 
 		// Helpers
@@ -47,5 +53,51 @@ func main() {
 		return
 	}
 
-	cli.Execute()
+	err = container.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ch, err := container.Serve()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	stopCh := make(chan struct{}, 1)
+
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case e := <-ch:
+				err = container.Stop()
+				if err != nil {
+					log.Fatal(e.Error.Error() + ":" + err.Error())
+				}
+				log.Fatal(e.Error)
+			case <-sig:
+				err = container.Stop()
+				if err != nil {
+					log.Fatal(err)
+				}
+				return
+			case <-stopCh:
+				// timeout
+				err = container.Stop()
+				if err != nil {
+					log.Fatal(err)
+				}
+				return
+			}
+		}
+	}()
+
+	stopCh <- struct{}{}
+	wg.Wait()
 }
