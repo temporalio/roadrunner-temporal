18c21ff9319edaf6a9648b565657d0ef376d881d
Wolfy-J <wolfy.jd@gmail.com>
2021-01-07 12:15:26 +0300

    - added new tests

---
+++ test/src/Activity/SimpleActivity.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Temporal\Tests\Activity;
+
+use Temporal\Activity\ActivityInterface;
+use Temporal\Activity\ActivityMethod;
+use Temporal\DataConverter\Bytes;
+use Temporal\Tests\DTO\Message;
+use Temporal\Tests\DTO\User;
+
+#[ActivityInterface(prefix: "SimpleActivity.")]
+class SimpleActivity
+{
+    #[ActivityMethod]
+    public function echo(
+        string $input
+    ): string {
+        return strtoupper($input);
+    }
+
+    #[ActivityMethod]
+    public function lower(
+        string $input
+    ): string {
+        return strtolower($input);
+    }
+
+    #[ActivityMethod]
+    public function greet(
+        User $user
+    ): Message {
+        return new Message(sprintf("Hello %s <%s>", $user->name, $user->email));
+    }
+
+    #[ActivityMethod]
+    public function slow(
+        string $input
+    ): string {
+        sleep(2);
+
+        return strtolower($input);
+    }
+
+    #[ActivityMethod]
+    public function md5(
+        Bytes $input
+    ): string {
+        return md5($input->getData());
+    }
+}
