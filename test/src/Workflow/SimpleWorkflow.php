5908ce95ae4515516dfa77140774116b8ceb11ee
Wolfy-J <wolfy.jd@gmail.com>
2021-01-08 17:25:15 +0300

    - added temporary proto alias

--- test/src/Workflow/SimpleWorkflow.php
+++ test/src/Workflow/SimpleWorkflow.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Temporal\Tests\Workflow;
 
 use Temporal\Activity\ActivityOptions;
+use Temporal\Api\Common\V1\WorkflowExecution;
 use Temporal\Workflow;
 use Temporal\Workflow\WorkflowMethod;
 use Temporal\Tests\Activity\SimpleActivity;
@@ -18,6 +19,8 @@ class SimpleWorkflow
             SimpleActivity::class,
             ActivityOptions::new()->withStartToCloseTimeout(5)
         );
+
+        error_log(get_class(new WorkflowExecution()));
 
         return yield $simple->echo($input);
     }
