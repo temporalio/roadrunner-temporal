70621f766b4e6f94cd34050b0550fe91284290fb
Valery Piashchynski <piashchynski.valery@gmail.com>
2024-02-29 15:30:54 +0100

    chore: more tests
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- tests/updates/updates_async_test.go
+++ tests/updates/updates_async_test.go
@@ -1,1 +1,266 @@
 package updates
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"testing"
+	"tests/helpers"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"go.temporal.io/api/enums/v1"
+	"go.temporal.io/api/history/v1"
+	updatepb "go.temporal.io/api/update/v1"
+	"go.temporal.io/sdk/client"
+)
+
+const (
+	awaitWithTimeoutM = "awaitWithTimeout"
+	awaitM            = "await"
+	resolveValueM     = "resolveValue"
+	// signal
+	exitSignal = "exit"
+	// queryResult
+	getValueQuery = "getValue"
+	// WF names
+	awaitsUpdateGreetWF = "AwaitsUpdate.greet"
+)
+
+func Test_Updates_9(t *testing.T) {
+	stopCh := make(chan struct{}, 1)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	s := helpers.NewTestServer(t, stopCh, wg, "../configs/.rr-proto.yaml")
+
+	w, err := s.Client.ExecuteWorkflow(
+		context.Background(),
+		client.StartWorkflowOptions{
+			TaskQueue: "default",
+		},
+		awaitsUpdateGreetWF)
+
+	require.NoError(t, err)
+	time.Sleep(time.Second)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	handle, err := s.Client.UpdateWorkflowWithOptions(ctx, &client.UpdateWorkflowWithOptionsRequest{
+		RunID:      w.GetRunID(),
+		WorkflowID: w.GetID(),
+		UpdateName: awaitWithTimeoutM,
+		Args:       []any{"key", 1, "fallback"},
+		WaitPolicy: &updatepb.WaitPolicy{
+			LifecycleStage: enums.UPDATE_WORKFLOW_EXECUTION_LIFECYCLE_STAGE_ACCEPTED,
+		},
+	})
+	require.NoError(t, err)
+
+	var result any
+
+	err = handle.Get(context.Background(), &result)
+	require.NoError(t, err)
+	require.Equal(t, "fallback", result.(string))
+
+	err = s.Client.SignalWorkflow(context.Background(), w.GetID(), w.GetRunID(), exitSignal, nil)
+	require.NoError(t, err)
+	time.Sleep(time.Second)
+
+	err = s.Client.GetWorkflow(ctx, w.GetID(), w.GetRunID()).Get(context.Background(), &result)
+	require.NoError(t, err)
+	require.Equal(t, map[string]any{"key": "fallback"}, result.(map[string]any))
+
+	s.AssertContainsEvent(s.Client, t, w, func(event *history.HistoryEvent) bool {
+		return event.EventType == enums.EVENT_TYPE_WORKFLOW_EXECUTION_SIGNALED
+	})
+
+	stopCh <- struct{}{}
+	wg.Wait()
+}
+
+func Test_Updates_10(t *testing.T) {
+	stopCh := make(chan struct{}, 1)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	s := helpers.NewTestServer(t, stopCh, wg, "../configs/.rr-proto.yaml")
+
+	w, err := s.Client.ExecuteWorkflow(
+		context.Background(),
+		client.StartWorkflowOptions{
+			TaskQueue: "default",
+		},
+		awaitsUpdateGreetWF)
+
+	require.NoError(t, err)
+	time.Sleep(time.Second)
+
+	wg.Add(1)
+	go func() {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+		defer cancel()
+
+		handle, err2 := s.Client.UpdateWorkflowWithOptions(ctx, &client.UpdateWorkflowWithOptionsRequest{
+			RunID:      w.GetRunID(),
+			WorkflowID: w.GetID(),
+			UpdateName: awaitM,
+			Args:       []any{"key"},
+			WaitPolicy: &updatepb.WaitPolicy{
+				LifecycleStage: enums.UPDATE_WORKFLOW_EXECUTION_LIFECYCLE_STAGE_ACCEPTED,
+			},
+		})
+		require.NoError(t, err2)
+
+		var result any
+		err2 = handle.Get(context.Background(), &result)
+		require.NoError(t, err2)
+		require.Equal(t, "resolved", result.(string))
+		wg.Done()
+	}()
+
+	time.Sleep(time.Second * 3)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	queryResult, err := s.Client.QueryWorkflow(ctx, w.GetID(), w.GetRunID(), getValueQuery, "key")
+	require.NoError(t, err)
+
+	var queryRes string
+	err = queryResult.Get(&queryRes)
+	require.NoError(t, err)
+	require.Equal(t, "", queryRes)
+
+	ctx3, cancel3 := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel3()
+
+	handle, err := s.Client.UpdateWorkflowWithOptions(ctx3, &client.UpdateWorkflowWithOptionsRequest{
+		RunID:      w.GetRunID(),
+		WorkflowID: w.GetID(),
+		UpdateName: resolveValueM,
+		Args:       []any{"key", "resolved"},
+		WaitPolicy: &updatepb.WaitPolicy{
+			LifecycleStage: enums.UPDATE_WORKFLOW_EXECUTION_LIFECYCLE_STAGE_ACCEPTED,
+		},
+	})
+	require.NoError(t, err)
+
+	var resultaw string
+	err = handle.Get(context.Background(), &resultaw)
+	require.NoError(t, err)
+	require.Equal(t, "resolved", resultaw)
+	wg.Done()
+
+	queryResult2, err := s.Client.QueryWorkflow(ctx, w.GetID(), w.GetRunID(), getValueQuery, "key")
+	require.NoError(t, err)
+
+	var queryRes2 string
+	err = queryResult2.Get(&queryRes2)
+	require.NoError(t, err)
+	require.Equal(t, "resolved", queryRes2)
+
+	err = s.Client.SignalWorkflow(context.Background(), w.GetID(), w.GetRunID(), exitSignal, nil)
+	require.NoError(t, err)
+	time.Sleep(time.Second)
+
+	var wfresult any
+	err = s.Client.GetWorkflow(ctx, w.GetID(), w.GetRunID()).Get(context.Background(), &wfresult)
+	require.NoError(t, err)
+	require.Equal(t, map[string]any{"key": "resolved"}, wfresult.(map[string]any))
+
+	s.AssertContainsEvent(s.Client, t, w, func(event *history.HistoryEvent) bool {
+		return event.EventType == enums.EVENT_TYPE_WORKFLOW_EXECUTION_SIGNALED
+	})
+
+	stopCh <- struct{}{}
+
+	wg.Wait()
+}
+
+func Test_Updates_11(t *testing.T) {
+	stopCh := make(chan struct{}, 1)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	s := helpers.NewTestServer(t, stopCh, wg, "../configs/.rr-proto.yaml")
+
+	w, err := s.Client.ExecuteWorkflow(
+		context.Background(),
+		client.StartWorkflowOptions{
+			TaskQueue: "default",
+		},
+		awaitsUpdateGreetWF)
+
+	require.NoError(t, err)
+	time.Sleep(time.Second)
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+			defer cancel()
+
+			handle, err2 := s.Client.UpdateWorkflowWithOptions(ctx, &client.UpdateWorkflowWithOptionsRequest{
+				RunID:      w.GetRunID(),
+				WorkflowID: w.GetID(),
+				UpdateName: awaitM,
+				Args:       []any{fmt.Sprintf("key-%d", i)},
+				WaitPolicy: &updatepb.WaitPolicy{
+					LifecycleStage: enums.UPDATE_WORKFLOW_EXECUTION_LIFECYCLE_STAGE_ACCEPTED,
+				},
+			})
+			require.NoError(t, err2)
+
+			var result any
+			err2 = handle.Get(context.Background(), &result)
+			require.NoError(t, err2)
+			require.Equal(t, "resolved", result.(string))
+			wg.Done()
+		}(i)
+	}
+
+	time.Sleep(time.Second * 3)
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+			defer cancel()
+
+			handle, err2 := s.Client.UpdateWorkflowWithOptions(ctx, &client.UpdateWorkflowWithOptionsRequest{
+				RunID:      w.GetRunID(),
+				WorkflowID: w.GetID(),
+				UpdateName: resolveValueM,
+				Args:       []any{fmt.Sprintf("key-%d", i), "resolved"},
+				WaitPolicy: &updatepb.WaitPolicy{
+					LifecycleStage: enums.UPDATE_WORKFLOW_EXECUTION_LIFECYCLE_STAGE_ACCEPTED,
+				},
+			})
+			require.NoError(t, err2)
+
+			var result any
+			err2 = handle.Get(context.Background(), &result)
+			require.NoError(t, err2)
+			require.Equal(t, "resolved", result.(string))
+			wg.Done()
+		}(i)
+	}
+
+	time.Sleep(time.Second * 3)
+
+	err = s.Client.SignalWorkflow(context.Background(), w.GetID(), w.GetRunID(), exitSignal, nil)
+	require.NoError(t, err)
+	time.Sleep(time.Second)
+
+	var wfresult any
+	err = s.Client.GetWorkflow(context.Background(), w.GetID(), w.GetRunID()).Get(context.Background(), &wfresult)
+	require.NoError(t, err)
+	res := map[string]any{"key-0": "resolved", "key-1": "resolved", "key-2": "resolved", "key-3": "resolved", "key-4": "resolved"}
+	require.Equal(t, res, wfresult.(map[string]any))
+
+	s.AssertContainsEvent(s.Client, t, w, func(event *history.HistoryEvent) bool {
+		return event.EventType == enums.EVENT_TYPE_WORKFLOW_EXECUTION_SIGNALED
+	})
+
+	stopCh <- struct{}{}
+
+	wg.Wait()
+}
--- tests/updates/updates_test.go
+++ tests/updates/updates_test.go
@@ -13,6 +13,18 @@ import (
 	"go.temporal.io/sdk/client"
 )
 
+const (
+	addNameM             = "addName"
+	addNameWOValidationM = "addNameWithoutValidation"
+	throwExcM            = "throwException"
+	randomizeNameM       = "randomizeName"
+	addNameViaActivityM  = "addNameViaActivity"
+	// signal
+	exitSig = "exit"
+	// WF names
+	updateGreetWF = "Update.greet"
+)
+
 func Test_UpdatesInit(t *testing.T) {
 	stopCh := make(chan struct{}, 1)
 	wg := &sync.WaitGroup{}
@@ -24,7 +36,7 @@ func Test_UpdatesInit(t *testing.T) {
 		client.StartWorkflowOptions{
 			TaskQueue: "default",
 		},
-		"Update.greet")
+		updateGreetWF)
 
 	require.NoError(t, err)
 	time.Sleep(time.Second)
@@ -32,7 +44,7 @@ func Test_UpdatesInit(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	handle, err := s.Client.UpdateWorkflow(ctx, w.GetID(), w.GetRunID(), "addName", "John Doe")
+	handle, err := s.Client.UpdateWorkflow(ctx, w.GetID(), w.GetRunID(), addNameM, "John Doe")
 	require.NoError(t, err)
 
 	var result any
@@ -41,7 +53,7 @@ func Test_UpdatesInit(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, "Hello, John Doe!", result.(string))
 
-	err = s.Client.SignalWorkflow(context.Background(), w.GetID(), w.GetRunID(), "exit", nil)
+	err = s.Client.SignalWorkflow(context.Background(), w.GetID(), w.GetRunID(), exitSig, nil)
 	require.NoError(t, err)
 
 	time.Sleep(time.Second)
@@ -54,7 +66,7 @@ func Test_UpdatesInit(t *testing.T) {
 	wg.Wait()
 }
 
-func Test_UpdatesSideEffect(t *testing.T) {
+func Test_Updates_2(t *testing.T) {
 	stopCh := make(chan struct{}, 1)
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
@@ -65,7 +77,7 @@ func Test_UpdatesSideEffect(t *testing.T) {
 		client.StartWorkflowOptions{
 			TaskQueue: "default",
 		},
-		"Update.greet")
+		updateGreetWF)
 
 	require.NoError(t, err)
 	time.Sleep(time.Second)
@@ -73,7 +85,171 @@ func Test_UpdatesSideEffect(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	handle, err := s.Client.UpdateWorkflow(ctx, w.GetID(), w.GetRunID(), "randomizeName", 3)
+	handle, err := s.Client.UpdateWorkflow(ctx, w.GetID(), w.GetRunID(), addNameWOValidationM, "John Doe 42")
+	require.NoError(t, err)
+
+	var result any
+
+	err = handle.Get(context.Background(), &result)
+	require.NoError(t, err)
+	require.Equal(t, "Hello, John Doe 42!", result.(string))
+
+	err = s.Client.SignalWorkflow(context.Background(), w.GetID(), w.GetRunID(), exitSig, nil)
+	require.NoError(t, err)
+
+	time.Sleep(time.Second)
+
+	s.AssertContainsEvent(s.Client, t, w, func(event *history.HistoryEvent) bool {
+		return event.EventType == enums.EVENT_TYPE_WORKFLOW_EXECUTION_SIGNALED
+	})
+
+	stopCh <- struct{}{}
+	wg.Wait()
+}
+
+func Test_Updates_4(t *testing.T) {
+	stopCh := make(chan struct{}, 1)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	s := helpers.NewTestServer(t, stopCh, wg, "../configs/.rr-proto.yaml")
+
+	w, err := s.Client.ExecuteWorkflow(
+		context.Background(),
+		client.StartWorkflowOptions{
+			TaskQueue: "default",
+		},
+		updateGreetWF)
+
+	require.NoError(t, err)
+	time.Sleep(time.Second)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	handle, err := s.Client.UpdateWorkflow(ctx, w.GetID(), w.GetRunID(), addNameM, "42")
+	require.NoError(t, err)
+
+	var result any
+
+	err = handle.Get(context.Background(), &result)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "Name must not contain digits")
+
+	err = s.Client.SignalWorkflow(context.Background(), w.GetID(), w.GetRunID(), exitSig, nil)
+	require.NoError(t, err)
+
+	time.Sleep(time.Second)
+
+	s.AssertContainsEvent(s.Client, t, w, func(event *history.HistoryEvent) bool {
+		return event.EventType == enums.EVENT_TYPE_WORKFLOW_EXECUTION_SIGNALED
+	})
+
+	stopCh <- struct{}{}
+	wg.Wait()
+}
+
+func Test_Updates_5(t *testing.T) {
+	stopCh := make(chan struct{}, 1)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	s := helpers.NewTestServer(t, stopCh, wg, "../configs/.rr-proto.yaml")
+
+	w, err := s.Client.ExecuteWorkflow(
+		context.Background(),
+		client.StartWorkflowOptions{
+			TaskQueue: "default",
+		},
+		updateGreetWF)
+
+	require.NoError(t, err)
+	time.Sleep(time.Second)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	handle, err := s.Client.UpdateWorkflow(ctx, w.GetID(), w.GetRunID(), throwExcM, "John Doe")
+	require.NoError(t, err)
+
+	var result any
+
+	err = handle.Get(context.Background(), &result)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "Test exception with John Doe")
+
+	err = s.Client.SignalWorkflow(context.Background(), w.GetID(), w.GetRunID(), exitSig, nil)
+	require.NoError(t, err)
+
+	time.Sleep(time.Second)
+
+	s.AssertContainsEvent(s.Client, t, w, func(event *history.HistoryEvent) bool {
+		return event.EventType == enums.EVENT_TYPE_WORKFLOW_EXECUTION_SIGNALED
+	})
+
+	stopCh <- struct{}{}
+	wg.Wait()
+}
+
+func Test_Updates_6(t *testing.T) {
+	stopCh := make(chan struct{}, 1)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	s := helpers.NewTestServer(t, stopCh, wg, "../configs/.rr-proto.yaml")
+
+	w, err := s.Client.ExecuteWorkflow(
+		context.Background(),
+		client.StartWorkflowOptions{
+			TaskQueue: "default",
+		},
+		updateGreetWF)
+
+	require.NoError(t, err)
+	time.Sleep(time.Second)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	handle, err := s.Client.UpdateWorkflow(ctx, w.GetID(), w.GetRunID(), randomizeNameM, 1)
+	require.NoError(t, err)
+
+	var result []any
+
+	err = handle.Get(context.Background(), &result)
+	require.NoError(t, err)
+	require.Len(t, result, 1)
+
+	err = s.Client.SignalWorkflow(context.Background(), w.GetID(), w.GetRunID(), exitSig, nil)
+	require.NoError(t, err)
+
+	time.Sleep(time.Second)
+
+	s.AssertContainsEvent(s.Client, t, w, func(event *history.HistoryEvent) bool {
+		return event.EventType == enums.EVENT_TYPE_WORKFLOW_EXECUTION_SIGNALED
+	})
+
+	stopCh <- struct{}{}
+	wg.Wait()
+}
+
+func Test_Updates_7(t *testing.T) {
+	stopCh := make(chan struct{}, 1)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	s := helpers.NewTestServer(t, stopCh, wg, "../configs/.rr-proto.yaml")
+
+	w, err := s.Client.ExecuteWorkflow(
+		context.Background(),
+		client.StartWorkflowOptions{
+			TaskQueue: "default",
+		},
+		updateGreetWF)
+
+	require.NoError(t, err)
+	time.Sleep(time.Second)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	handle, err := s.Client.UpdateWorkflow(ctx, w.GetID(), w.GetRunID(), randomizeNameM, 3)
 	require.NoError(t, err)
 
 	var result []any
@@ -82,13 +258,58 @@ func Test_UpdatesSideEffect(t *testing.T) {
 	require.NoError(t, err)
 	require.Len(t, result, 3)
 
-	err = s.Client.SignalWorkflow(context.Background(), w.GetID(), w.GetRunID(), "exit", nil)
+	err = s.Client.SignalWorkflow(context.Background(), w.GetID(), w.GetRunID(), exitSig, nil)
 	require.NoError(t, err)
 
 	time.Sleep(time.Second)
 
 	s.AssertContainsEvent(s.Client, t, w, func(event *history.HistoryEvent) bool {
 		return event.EventType == enums.EVENT_TYPE_WORKFLOW_EXECUTION_SIGNALED
+	})
+
+	stopCh <- struct{}{}
+	wg.Wait()
+}
+
+func Test_Updates_8(t *testing.T) {
+	stopCh := make(chan struct{}, 1)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	s := helpers.NewTestServer(t, stopCh, wg, "../configs/.rr-proto.yaml")
+
+	w, err := s.Client.ExecuteWorkflow(
+		context.Background(),
+		client.StartWorkflowOptions{
+			TaskQueue: "default",
+		},
+		updateGreetWF)
+
+	require.NoError(t, err)
+	time.Sleep(time.Second)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	handle, err := s.Client.UpdateWorkflow(ctx, w.GetID(), w.GetRunID(), addNameViaActivityM, "John Doe")
+	require.NoError(t, err)
+
+	var result any
+
+	err = handle.Get(context.Background(), &result)
+	require.NoError(t, err)
+	require.Equal(t, "Hello, john doe!", result.(string))
+
+	err = s.Client.SignalWorkflow(context.Background(), w.GetID(), w.GetRunID(), exitSig, nil)
+	require.NoError(t, err)
+
+	time.Sleep(time.Second)
+
+	s.AssertContainsEvent(s.Client, t, w, func(event *history.HistoryEvent) bool {
+		return event.EventType == enums.EVENT_TYPE_WORKFLOW_EXECUTION_SIGNALED
+	})
+
+	s.AssertContainsEvent(s.Client, t, w, func(event *history.HistoryEvent) bool {
+		return event.EventType == enums.EVENT_TYPE_ACTIVITY_TASK_COMPLETED
 	})
 
 	stopCh <- struct{}{}
