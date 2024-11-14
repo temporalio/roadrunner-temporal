d181d53ebe9ba3fe10660b08162b49bcd4041dd2
Kajetan <cv65kr@gmail.com>
2024-11-14 22:49:58 +0100

    Support TLS without certifactes

--- config.go
+++ config.go
@@ -138,20 +138,24 @@ func (c *Config) InitDefault() error {
 	}
 
 	if c.TLS != nil {
-		if _, err := os.Stat(c.TLS.Key); err != nil {
-			if os.IsNotExist(err) {
-				return errors.E(op, errors.Errorf("private key file '%s' does not exist", c.TLS.Key))
-			}
+		if c.TLS.Key != "" {
+			if _, err := os.Stat(c.TLS.Key); err != nil {
+				if os.IsNotExist(err) {
+					return errors.E(op, errors.Errorf("private key file '%s' does not exist", c.TLS.Key))
+				}
 
-			return errors.E(op, err)
+				return errors.E(op, err)
+			}
 		}
 
-		if _, err := os.Stat(c.TLS.Cert); err != nil {
-			if os.IsNotExist(err) {
-				return errors.E(op, errors.Errorf("public certificate file '%s' does not exist", c.TLS.Cert))
-			}
+		if c.TLS.Cert != "" {
+			if _, err := os.Stat(c.TLS.Cert); err != nil {
+				if os.IsNotExist(err) {
+					return errors.E(op, errors.Errorf("public certificate file '%s' does not exist", c.TLS.Cert))
+				}
 
-			return errors.E(op, err)
+				return errors.E(op, err)
+			}
 		}
 
 		// RootCA is optional, but if provided - check it
---
+++ tests/general/h2c_test.go
@@ -0,0 +1,52 @@
+package tests
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	"tests/helpers"
+
+	"github.com/stretchr/testify/assert"
+	"go.temporal.io/sdk/client"
+)
+
+func Test_H2CWithoutCerts(t *testing.T) {
+	stopCh := make(chan struct{}, 1)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	s := helpers.NewTestServer(t, stopCh, wg, "../configs/.rr-h2c.yaml")
+
+	w, err := s.Client.ExecuteWorkflow(
+		context.Background(),
+		client.StartWorkflowOptions{
+			TaskQueue: "default",
+		},
+		"HistoryLengthWorkflow")
+	assert.NoError(t, err)
+
+	time.Sleep(time.Second)
+	var result any
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+	assert.NoError(t, w.Get(ctx, &result))
+
+	res := []float64{3, 8, 8, 15}
+	out := result.([]interface{})
+
+	for i := 0; i < len(res); i++ {
+		if res[i] != out[i].(float64) {
+			t.Fail()
+		}
+	}
+
+	we, err := s.Client.DescribeWorkflowExecution(context.Background(), w.GetID(), w.GetRunID())
+	assert.NoError(t, err)
+
+	assert.Equal(t, "Completed", we.WorkflowExecutionInfo.Status.String())
+	stopCh <- struct{}{}
+	wg.Wait()
+	time.Sleep(time.Second)
+}
