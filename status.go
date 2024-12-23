d7f31f7d40c2ad5fc2ae615f064aa2122c962013
Kajetan <cv65kr@gmail.com>
2024-12-23 14:16:44 +0100

    Fix status of Temporal

--- status.go
+++ status.go
@@ -3,8 +3,9 @@ package rrtemporal
 import (
 	"net/http"
 
-	"github.com/roadrunner-server/api/v4/plugins/v1/status"
 	"github.com/roadrunner-server/pool/fsm"
+
+	"github.com/roadrunner-server/api/v4/plugins/v1/status"
 )
 
 // Status return status of the particular plugin
@@ -12,7 +13,7 @@ func (p *Plugin) Status() (*status.Status, error) {
 	p.mu.RLock()
 	defer p.mu.RUnlock()
 
-	if p.config.DisableActivityWorkers && p.wfP.Workers()[0].State().IsActive() {
+	if p.config.DisableActivityWorkers && len(p.wfP.Workers()) > 0 && p.wfP.Workers()[0].State().IsActive() {
 		return &status.Status{
 			Code: http.StatusOK,
 		}, nil
@@ -38,7 +39,7 @@ func (p *Plugin) Ready() (*status.Status, error) {
 	p.mu.RLock()
 	defer p.mu.RUnlock()
 
-	if p.config.DisableActivityWorkers && p.wfP.Workers()[0].State().Compare(fsm.StateReady) {
+	if p.config.DisableActivityWorkers && len(p.wfP.Workers()) > 0 && p.wfP.Workers()[0].State().Compare(fsm.StateReady) {
 		return &status.Status{
 			Code: http.StatusOK,
 		}, nil
