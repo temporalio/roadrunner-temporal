f0c0bffa5f6256dc537e4176c2df3ab71ed5b6e7
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-09-04 21:39:14 +0200

    chore: use zap.Any to print worker_info debug output
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- aggregatedpool/workers.go
+++ aggregatedpool/workers.go
@@ -19,38 +19,7 @@ func TemporalWorkers(wDef *Workflow, actDef *Activity, wi []*internal.WorkerInfo
 	workers := make([]worker.Worker, 0, 1)
 
 	for i := range wi {
-		log.Debug("worker info",
-			zap.String(tq, wi[i].TaskQueue),
-			zap.Any("flags", wi[i].Flags),
-			zap.Int("num_workflows", len(wi[i].Workflows)),
-			zap.Int("num_activities", len(wi[i].Activities)),
-			zap.Any("workflows", wi[i].Workflows),
-			zap.Any("activities", wi[i].Activities),
-			zap.Int("max_concurrent_activity_execution_size", wi[i].Options.MaxConcurrentActivityExecutionSize),
-			zap.Float64("worker_activities_per_second", wi[i].Options.WorkerActivitiesPerSecond),
-			zap.Int("max_concurrent_local_activity_execution_size", wi[i].Options.MaxConcurrentLocalActivityExecutionSize),
-			zap.Float64("worker_local_activities_per_second", wi[i].Options.WorkerLocalActivitiesPerSecond),
-			zap.Float64("task_queue_activities_per_second", wi[i].Options.TaskQueueActivitiesPerSecond),
-			zap.Int("max_concurrent_activity_task_pollers", wi[i].Options.MaxConcurrentActivityTaskPollers),
-			zap.Int("max_concurrent_workflow_task_pollers", wi[i].Options.MaxConcurrentWorkflowTaskPollers),
-			zap.Bool("enable_logging_in_replay", wi[i].Options.EnableLoggingInReplay),
-			zap.String("workflow_panic_policy", policyToString(wi[i].Options.WorkflowPanicPolicy)),
-			zap.Duration("worker_stop_timeout", wi[i].Options.WorkerStopTimeout),
-			zap.Bool("enable_session_worker", wi[i].Options.EnableSessionWorker),
-			zap.Int("max_concurrent_session_execution_size", wi[i].Options.MaxConcurrentSessionExecutionSize),
-			zap.Bool("disable_workflow_worker", wi[i].Options.DisableWorkflowWorker),
-			zap.Bool("local_activity_worker_only", wi[i].Options.LocalActivityWorkerOnly),
-			zap.String("identity", wi[i].Options.Identity),
-			zap.Duration("deadlock_detection_timeout", wi[i].Options.DeadlockDetectionTimeout),
-			zap.Duration("max_heartbeat_throttle_interval", wi[i].Options.MaxHeartbeatThrottleInterval),
-			zap.Duration("default_heartbeat_throttle_interval", wi[i].Options.DefaultHeartbeatThrottleInterval),
-			zap.Bool("disable_eager_activities", wi[i].Options.DisableEagerActivities),
-			zap.Int("max_concurrent_eager_activity_execution_size", wi[i].Options.MaxConcurrentEagerActivityExecutionSize),
-			zap.Bool("disable_registration_aliasing", wi[i].Options.DisableRegistrationAliasing),
-			zap.Bool("use_versioning", wi[i].Options.DeploymentOptions.UseVersioning),
-			zap.String("build_id", wi[i].Options.DeploymentOptions.Version.BuildId),
-			zap.String("deployment_name", wi[i].Options.DeploymentOptions.Version.DeploymentName),
-		)
+		log.Debug("worker info", zap.Any("worker_info", wi[i]))
 
 		// just to be sure
 		wi[i].Options.WorkerStopTimeout = 0
@@ -108,15 +77,4 @@ func TemporalWorkers(wDef *Workflow, actDef *Activity, wi []*internal.WorkerInfo
 	log.Debug("workers initialized", zap.Int("num_workers", len(workers)))
 
 	return workers, nil
-}
-
-func policyToString(enum worker.WorkflowPanicPolicy) string {
-	switch enum {
-	case worker.BlockWorkflow:
-		return "block"
-	case worker.FailWorkflow:
-		return "fail"
-	default:
-		return "unknown"
-	}
 }
